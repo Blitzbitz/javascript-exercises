86324ab44459f99c9a9a85bc402bf2445bd7191f
Blitzbitz <165694529+Blitzbitz@users.noreply.github.com>
2024-10-03 15:22:24 +0100

    tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let repeatedString = ``;
+    if(num >= 0){
+        for(let i = 0; i < num; i++){
+        repeatedString += string;
+    }
+    return repeatedString;
+    }else{return 'ERROR';}
 
 };
 
